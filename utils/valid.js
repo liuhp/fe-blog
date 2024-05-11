b9b1b8a24d1c470544d9014f9c343bd78e88244a
liuhp <2190098961@qq.com>
2024-05-11 11:00:37 +0800

    Create valid.js

---
+++ utils/valid.js
@@ -0,0 +1,14 @@
+```
+// 实现禁止输入特殊字符,并且不超多number字符长度
+function(value, number = 255) {
+  value = value.replace(/[`~!@#$%^&*()_\-+=<>?:"{}|,./;'\\[\]·~！@#￥%……&*（）——\-+={}|《》？：“”【】、；‘’，。、]/g, '').replace(/\s/g, '')
+  if (value.length >= number) {
+    value = value.slice(0, number)
+    this.$message({
+      type: 'warning',
+      message: `输入内容不能超过${number}个字符`
+    })
+  }
+  return value
+}
+```
